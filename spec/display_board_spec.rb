b317e205e6edd751979ac153c07a13894ebbe17c
Hemmy <c.hemant.c@gmail.com>
2020-11-11 19:08:40 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,14 +137,14 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] 
 
-      
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
-          
+
       expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" X | X | X ")
