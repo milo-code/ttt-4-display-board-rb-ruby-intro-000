a7284e5b34496d46395bc586f6a3179b3db983d3
Hemmy <c.hemant.c@gmail.com>
2020-11-11 19:38:39 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -23,7 +23,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       expect(rows[0]).to eq("   |   |   ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   | X |   ")
+      expect(rows[2]).to eq("   |   |   ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq("   |   |   ")
 
